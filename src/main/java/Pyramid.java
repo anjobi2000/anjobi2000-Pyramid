4274ac9d9b8f328837b24eda0382b5701807ba1f
Angela Jobi <Angela.chemmazham@gmail.com>
2025-04-12 15:33:57 +0000

    Completed Coding Challenge Pyramid

--- src/main/java/Pyramid.java
+++ src/main/java/Pyramid.java
@@ -28,6 +28,11 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
-        return "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            sb.append("*".repeat(i));
+            sb.append("\n");
+        } 
+        return sb.toString();
     }
 }
